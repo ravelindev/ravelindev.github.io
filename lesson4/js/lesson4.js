8295e40a210269ac324448beabe8dd87bcd81a11
ravelindev <ravelindev@gmail.com>
2020-05-19 05:00:07 -0400

    Fixing my page

--- lesson4/js/lesson4.js
+++ lesson4/js/lesson4.js
@@ -1,2 +1,10 @@
-let time = document.lastModified;
-document.getElementById("lastUpdate").innerHTML = time;
+var d = new Date();
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("myDate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " +
+month[d.getMonth()] + " " + d.getFullYear();
+
+function toggleMenu() {
+    console.log(document.getElementById('primaryNav').classList);
+    document.getElementById('primaryNav').classList.toggle('hide');
+}
